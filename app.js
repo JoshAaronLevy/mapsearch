45e921d2bdde5a23fa1cf33bc34610c9ce7e33df
Josh Levy <joshaaronlevy@gmail.com>
2019-07-16 02:20:08 -0600

    Got map rendering on DOM

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+const home = [39.759541,-104.999107];
+const map = L.map("map").setView(home, 17);
 
-// var map = L.map('map', {
-//   center: [51.505, -0.09],
-//   zoom: 13
-// });
+L.marker(home)
+    .bindPopup("Home")
+    .openPopup()
+    .addTo(map);
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
+}).addTo(map);
