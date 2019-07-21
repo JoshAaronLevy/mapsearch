963fb1921a8e64846b143ccbb20c9438342910d5
Josh Levy <joshaaronlevy@gmail.com>
2019-07-21 02:16:49 -0600

    Got draw square working

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const home = [39.759541,-104.999107];
-const map = L.map("map").setView(home, 17);
-
-L.marker(home)
-    .bindPopup("Home")
-    .openPopup()
-    .addTo(map);
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
+const home = [39.759541, -104.999107];
+var map = L.map('map').setView(home, 17);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+var options = {
+  position: 'topleft',
+  draw: {
+    polyline: false,
+    polygon: false,
+    circle: false,
+    circlemarker: false,
+    rectangle: {
+      shapeOptions: {
+        clickable: false
+      }
+    },
+    marker: false
+  },
+  edit: false
+};
+
+var drawControl = new L.Control.Draw(options);
+map.addControl(drawControl);
+
+map.on(L.Draw.Event.CREATED, function(e) {
+  var type = e.layerType,
+    layer = e.layer;
+
+  if (type === 'marker') {
+    layer.bindPopup('A popup!');
+  }
+});
