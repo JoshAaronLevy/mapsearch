4d3506de84cf217862c6ea9e6a4e44c048c8179f
Josh Levy <joshaaronlevy@gmail.com>
2019-07-21 04:11:56 -0600

    Got lat and lng coordinates firing for both mousedown and mouseup. Need to isolate it for drawing only

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const home = [39.759541, -104.999107];
-var map = L.map('map').setView(home, 17);
+let map = L.map('map').setView(home, 17);
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var editableLayers = new L.FeatureGroup();
+let editableLayers = new L.FeatureGroup();
 map.addLayer(editableLayers);
 
-var MyCustomMarker = L.Icon.extend({
+let MyCustomMarker = L.Icon.extend({
   options: {
     shadowUrl: null,
     iconAnchor: new L.Point(12, 12),
@@ -17,7 +17,7 @@ var MyCustomMarker = L.Icon.extend({
   }
 });
 
-var drawControl = new L.Control.Draw({
+let drawControl = new L.Control.Draw({
   position: 'topleft',
   draw: {
     polyline: false,
@@ -36,10 +36,30 @@ var drawControl = new L.Control.Draw({
 map.addControl(drawControl);
 
 map.on(L.Draw.Event.CREATED, function(e) {
-  var type = e.layerType,
+  let type = e.layerType,
     layer = e.layer;
   if (type === 'marker') {
     layer.bindPopup('Marker');
   }
   editableLayers.addLayer(layer);
+});
+
+let beginDraw = new L.Control.Coordinates()
+
+beginDraw.addTo(map);
+
+map.on('mousedown', function(e) {
+  beginDraw.setCoordinates(e);
+  console.log('Beginning coordinates:')
+  console.log(e.latlng.lat, e.latlng.lng)
+});
+
+let endDraw = new L.Control.Coordinates()
+
+endDraw.addTo(map);
+
+map.on('mouseup', function(e) {
+  endDraw.setCoordinates(e);
+  console.log('Ending coordinates:')
+  console.log(e.latlng.lat, e.latlng.lng)
 });
